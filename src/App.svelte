7c847ad7a0007b3ee9dfb9c35e20f8557f502d98
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2020-07-21 14:14:23 +0200

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -15,18 +15,28 @@
 		chart = chart.filter(i => i[field]); //check that field is set
 		let dates = chart.map((i) => new Date(i[field]["@value"]));
 		dates = dates.filter(i => i instanceof Date && !isNaN(i)); //filter for true dates
-		const minDate = new Date(Math.min.apply(null,dates));
+		const minDate = new Date(Math.min.apply(null,dates)); //get the earliest date in the array
 		let xAxis = [];
-		for (let d=minDate.getTime(); d < Date.now();d += (60 * 60 * 24 * 1000) ){
+		for (let d=minDate.getTime(); d < Date.now();d += (60 * 60 * 24 * 1000) ){ //create a field for every day from min to now
 			xAxis.push(new Date(d));
 		}
-		const yAxis = xAxis.map((x) => dates.filter((d) => d.getTime() <= x.getTime()).length);
+		const yAxis = xAxis.map((x) => dates.filter((d) => d.getTime() <= x.getTime()).length); //get a value for every day.
+		const yBar = xAxis.map(x => dates.filter(d => d.getDate() === x.getDate() && d.getMonth() === x.getMonth()).length);
+		console.log(dates[33].getDay());
+		console.log(yBar);
 		return {
 			labels: xAxis.map(d => d.toLocaleDateString()),
 			datasets: [
 				{
-					values: yAxis
-				}
+					name: "Beiträge Gesamt",
+					values: yAxis,
+					chartType: 'line'
+				},
+				{
+					name: "Beiträge Täglich",
+					values: yBar,
+					chartType: 'bar'
+				},
 			]
 		};
 	};
@@ -51,6 +61,7 @@
 			labels: xAxis,
 			datasets: [
 				{
+					name: "Beiträge",
 					values: yAxis
 				}
 			]
@@ -59,7 +70,7 @@
 
 	const wordchart = ( (oData) => {
 		oData = oData.filter(i => i['dcterms:abstract']);
-		const data = oData.map(i => i['dcterms:abstract'][0]['@value']).toString();
+		const data = [...oData.map(i => i['o:title']), ...oData.map(i => i['dcterms:abstract'][0]['@value'])].toString();
 
 		// create a chart and set the data
 		const wordChart = anychart.tagCloud();
@@ -122,7 +133,17 @@
 					"hat",
 					"ces",
 					"una",
-					"du"
+					"du",
+					"au",
+					"se",
+					"so",
+					"ad",
+					"an",
+					"on",
+					"ils",
+					"del",
+					"um",
+					"to"
 
 			]
 		});
@@ -140,16 +161,43 @@
 	{#await chart}
 		<p>...waiting</p>
 	{:then data}
-		<Chart data={graphVal(data, "o:created")} type="line" axisOptions={{xIsSeries:true}} />
-		<Chart data={pieVal(data, "dcterms:language")} type="pie" />
+		<h2>Gesamtbeiträge</h2>
+		<Chart data={graphVal(data, "o:created")} axisOptions={{xIsSeries:true}} type='axis-mixed' />
+		<p>Der Graph zeigt auf der X-Achse einen Zeitverlauf, auf der Y-Achse die Anzahl an Beiträgen auf Corona-Memory.ch</p>
+		<h2>Beiträge nach Sprachen</h2>
+	<div class="grid">
+		<div>
+			<h3>Beiträge nach Sprache</h3>
+			<Chart data={pieVal(data, "dcterms:language")} type="pie" />
+		</div>
+		<div>
+			<h3>Sprecher in der Schweiz</h3>
+			<Chart data={{
+				labels: ['de','fr','it'],
+				datasets: [
+					{
+						name: "Sprecher",
+						values: [4458156, 1619708, 593646]
+					}
+				]
+			}} type="pie" />
+		</div>
+	</div>
+		<p>Das Kuchendiagramm zeigt, in welcher Sprache unsere Beiträge verfasst sind. Als Referenz dazu sehen Sie die Zahlen der Sprecher der jeweiligen Sprachen in der Schweiz.(2018. Quelle: Bundesamt für Statistik)</p>
 	{:catch error}
 		<p style="color: red">{error.message}</p>
 	{/await}
+	<h2>Wörterwolke</h2>
 	<div id="container"></div>
+	<p>Die Wolke zeigt die meistbenutzten Wörter in den Beiträgen.</p>
 </main>
 
 <style>
 #container {
 	height: 800px;
 }
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
 </style>
